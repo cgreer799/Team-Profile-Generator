b1285845a4f706d0c89dc29a80c22cebf3673b4c
cgreer799 <cgreer799@gmail.com>
2022-12-03 16:42:06 -0600

    added engineer and intern functions

--- index.js
+++ index.js
@@ -50,12 +50,116 @@ function createManager () {
         employeesArray.push(manager);
         switch(inquirerResponses.addEmployees){
             case 'Intern':
-                //Future function to create intern
-                //createIntern();
+                //function to create intern
+                createIntern();
             break;
             case 'Engineer':
-                //Future function to create Engineer
-                //createEngineer();
+                //function to create Engineer
+                createEngineer();
+            break;
+            case 'Generate Team':
+            writeHTML();
+        };
+    })
+}
+
+function createEngineer () {
+    const engineerQuestions = [
+        {
+            type: 'input',
+            name: 'name',
+            message: "Provide the engineer's name.",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Provide the engineer's ID.",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Provide the engineer's email address.",
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "Provide the engineer's GitHub username.",
+        },
+        {
+            type: 'list',
+            name: 'addEmployees',
+            message: 'Select from the list to add another Employee. If you are done adding Employees, select Generate Team.',
+            choices: [
+                'Intern',
+                'Engineer',
+                'Generate Team'
+            ]
+        }
+    ];
+
+    inquirer.prompt(engineerQuestions).then((inquirerResponses) => {
+        const engineer = new Engineer (inquirerResponses.name, inquirerResponses.id, inquirerResponses.email, inquirerResponses.github);
+        employeesArray.push(engineer);
+        switch(inquirerResponses.addEmployees){
+            case 'Intern':
+                //function to create intern
+                createIntern();
+            break;
+            case 'Engineer':
+                //function to create Engineer
+                createEngineer();
+            break;
+            case 'Generate Team':
+            writeHTML();
+        };
+    })
+}
+
+function createIntern () {
+    const internQuestions = [
+        {
+            type: 'input',
+            name: 'name',
+            message: "Provide the intern's name.",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Provide the intern's ID.",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Provide the intern's email address.",
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "Provide the intern's school.",
+        },
+        {
+            type: 'list',
+            name: 'addEmployees',
+            message: 'Select from the list to add another Employee. If you are done adding Employees, select Generate Team.',
+            choices: [
+                'Intern',
+                'Engineer',
+                'Generate Team'
+            ]
+        }
+    ];
+
+    inquirer.prompt(internQuestions).then((inquirerResponses) => {
+        const intern = new Intern (inquirerResponses.name, inquirerResponses.id, inquirerResponses.email, inquirerResponses.school);
+        employeesArray.push(intern);
+        switch(inquirerResponses.addEmployees){
+            case 'Intern':
+                // function to create intern
+               createIntern();
+            break;
+            case 'Engineer':
+                // function to create Engineer
+                createEngineer();
             break;
             case 'Generate Team':
             writeHTML();
