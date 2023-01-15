dc3324291cc3584ffeeb3eaf4766587077cd8b77
cgreer799 <cgreer799@gmail.com>
2023-01-15 16:05:12 -0600

    added validation to inputs for manager and engineer

--- index.js
+++ index.js
@@ -16,31 +16,57 @@ function createManager () {
         {
             type: 'input',
             name: 'name',
-            message: "Provide the manager's name.",
+            message: "Enter the manager's name. (Name cannot be blank)",
+            validate: (answer) => {
+                if (answer !== "") {
+                  return true;
+                }
+                return "The manager's name must not be blank.";
+            }
         },
         {
             type: 'input',
             name: 'id',
-            message: "Provide the manager's ID.",
+            message: "Enter the manager's ID. (any number positive 1 to 3 digit number)",
+            validate: (answer) => {
+                const validAnswer = answer.match(/[0-9][0-9][0-9]/);
+                if (validAnswer) {
+                  return true;
+                }
+                return "The manager's id must be a 1 to 3 digit number.";
+            }
         },
         {
             type: 'input',
             name: 'email',
-            message: "Provide the manager's email address.",
+            message: "Enter the manager's email address.",
+            validate: (answer) => {
+                const validAnswer = answer.match(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/);
+                if (validAnswer) {
+                  return true;
+                }
+                return "Please enter a valid email for the manager.";
+            }
         },
         {
             type: 'input',
             name: 'officeNumber',
-            message: "Provide the manager's office number.",
+            message: "Enter the manager's office number. (3 Capital letters followed by 3 numbers. Ex: AAA123)",
+            validate: (answer) => {
+                const validAnswer = answer.match(/^([A-Z]{3})([0-9]{3})$/);
+                if (validAnswer) {
+                  return true;
+                }
+                return "Please enter a valid office number for the manager.";
+            }
         },
         {
             type: 'list',
             name: 'addEmployees',
-            message: 'Select from the list to add another Employee. If you are done adding Employees, select Generate Team.',
+            message: 'Select from the list to add another Employee to your team.',
             choices: [
                 'Intern',
-                'Engineer',
-                'Generate Team'
+                'Engineer'
             ]
         }
     ];
@@ -50,15 +76,10 @@ function createManager () {
         employeesArray.push(manager);
         switch(inquirerResponses.addEmployees){
             case 'Intern':
-                //function to create intern
                 createIntern();
             break;
             case 'Engineer':
-                //function to create Engineer
                 createEngineer();
-            break;
-            case 'Generate Team':
-            writeHTML();
         };
     })
 }
@@ -68,22 +89,61 @@ function createEngineer () {
         {
             type: 'input',
             name: 'name',
-            message: "Provide the engineer's name.",
+            message: "Enter the engineer's name.",
+            validate: (answer) => {
+                if (answer !== "") {
+                  return true;
+                }
+                return "The engineer's name must not be blank.";
+            }
         },
         {
             type: 'input',
             name: 'id',
             message: "Provide the engineer's ID.",
+            validate: (answer) => {
+                const validAnswer = answer.match(/[0-9][0-9][0-9]/);
+                if (validAnswer) {
+                    if (employeesArray.includes(answer)) {
+                        return 'This ID already exists in your team.';
+                    } else {
+                        return true;
+                    }
+                }
+                return "The engineer's id must be a 1 to 3 digit number.";
+            }
         },
         {
             type: 'input',
             name: 'email',
             message: "Provide the engineer's email address.",
+            validate: (answer) => {
+                const validAnswer = answer.match(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/);
+                if (validAnswer) {
+                    if (employeesArray.includes(answer)) {
+                        return 'This email already exists in your team.';
+                    } else {
+                        return true;
+                    }
+                }
+                return "Please enter a valid email for the engineer.";
+            }
         },
         {
             type: 'input',
             name: 'github',
             message: "Provide the engineer's GitHub username.",
+            validate: (answer) => {
+                const validAnswer = answer.match(/^([a-zA-Z0-9]*)/);
+                if (validAnswer) {
+                    if (employeesArray.includes(answer)) {
+                        return 'This GitHub username already exists in your team.';
+                    } else {
+                        return true;
+                    }
+                }
+                return "Please enter a valid email for the manager.";
+            }
         },
         {
             type: 'list',
@@ -102,15 +162,13 @@ function createEngineer () {
         employeesArray.push(engineer);
         switch(inquirerResponses.addEmployees){
             case 'Intern':
-                //function to create intern
                 createIntern();
             break;
             case 'Engineer':
-                //function to create Engineer
                 createEngineer();
             break;
             case 'Generate Team':
-            writeHTML();
+                writeHTML();
         };
     })
 }
